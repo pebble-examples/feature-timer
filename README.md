781e1327e849179c26759053dd90ca7adbb55325
C-D-Lewis <Bonsitm@gmail.com>
2015-03-11 14:32:51 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# feature-timer
+
+![screenshot](feature-timer-screenshot.png)
+
+This example shows how to use the [`Timer`](https://developer.getpebble.com/docs/c/group___timer.html).
---
+++ src/feature_timer.c
@@ -0,0 +1,45 @@
+#include <pebble.h>
+
+static Window *s_main_window;
+static TextLayer *s_text_layer;
+static AppTimer *s_timer;
+
+static void timer_callback(void *data) {
+  text_layer_set_text(s_text_layer, "Timer happened!");
+}
+
+static void main_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_frame(window_layer);
+
+  s_text_layer = text_layer_create(bounds);
+  text_layer_set_text(s_text_layer, "Waiting for timer...");
+  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+
+  s_timer = app_timer_register(1500, timer_callback, NULL);
+}
+
+static void main_window_unload(Window *window) {
+  text_layer_destroy(s_text_layer);
+}
+
+static void init() {
+  s_main_window = window_create();
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload,
+  });
+  window_stack_push(s_main_window, true);
+}
+
+static void deinit(void) {
+  window_destroy(s_main_window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
